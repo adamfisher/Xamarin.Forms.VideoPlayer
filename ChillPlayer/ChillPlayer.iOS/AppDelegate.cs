9b5530d05edeb9a457b9ca1a3bcb0edb72c1bdbf
Adam <fisher1987@gmail.com>
2018-01-08 23:27:48 -0500

    Cleaned up Chill Player example and fixed initialization on iOS.

--- ChillPlayer/ChillPlayer.iOS/AppDelegate.cs
+++ ChillPlayer/ChillPlayer.iOS/AppDelegate.cs
@@ -12,31 +12,10 @@ namespace ChillPlayer.iOS
     [Register("AppDelegate")]
     public class AppDelegate : FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-			#if ENABLE_TEST_CLOUD
-			// requires Xamarin Test Cloud Agent
-			Xamarin.Calabash.Start();
-			#endif
-
             Forms.Init();
-
-			// Test Cloud StyleId
-			Forms.ViewInitialized += (sender, e) => {
-				// http://developer.xamarin.com/recipes/testcloud/set-accessibilityidentifier-ios/
-				if (null != e.View.StyleId) {
-					e.NativeView.AccessibilityIdentifier = e.View.StyleId;
-				}
-			};
-
-            FormsVideoPlayer.Init("C9107D52F8CDFAB6C552D5CA415D9A926BCEE319");
+            FormsVideoPlayer.Init();
             LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
