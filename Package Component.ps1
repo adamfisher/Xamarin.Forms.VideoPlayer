243e81935ea2b81df2dcf5e8a340c88ba0a2be29
Adam <fisher1987@gmail.com>
2019-06-19 23:30:24 -0400

    Adding metadata

---
+++ Package Component.ps1
@@ -0,0 +1,5 @@
+﻿# Package Nuget
+dotnet pack src\Octane.Xamarin.Forms.VideoPlayer\Octane.Xamarin.Forms.VideoPlayer.csproj -c Release --no-build
+Move-Item src\Octane.Xamarin.Forms.VideoPlayer\bin\Release\*.nupkg -Destination . -force
+
+pause
